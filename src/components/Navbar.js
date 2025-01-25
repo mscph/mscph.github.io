b74702313bc800cfd24aae28245e0df5b2e09080
Jobert Tunguia <66238561+VonnAirone@users.noreply.github.com>
2025-01-25 19:08:24 +0800

    added navigation in mobile view

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,20 @@
-import React from 'react'
-import './Navbar.css'
-import { FaBars } from 'react-icons/fa6'
-import { NavLink } from 'react-router-dom'
+import React, { useState } from 'react';
+import './Navbar.css';
+import { FaBars, FaXmark } from 'react-icons/fa6';
+import { NavLink } from 'react-router-dom';
 
 export default function Navbar() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <nav className='flex h-20 items-center justify-between'>
         <div>
             <NavLink to={'/'}>
-                <img src='MSCLogo.jpg' height={60} width={60} />
+                <img src='MSCLogo.jpg' height={60} width={60} alt="MSC Logo" />
             </NavLink>
         </div>
 
@@ -23,9 +29,24 @@ export default function Navbar() {
             </ul>
         </div>
 
-        <div className='lg:hidden hover:cursor-pointer'>
-            <FaBars />
+        <div className='lg:hidden'>
+            <div onClick={toggleMobileMenu} className='hover:cursor-pointer'>
+                {isMobileMenuOpen ? <FaXmark size={24} /> : <FaBars size={24} />}
+            </div>
         </div>
+
+        {isMobileMenuOpen && (
+            <div className='absolute top-0 left-0 w-full bg-white p-4 shadow-md lg:hidden'>
+                <ul className='flex flex-col gap-5 text-lg'>
+                    <li><NavLink to={'/'} className={({ isActive }) => isActive ? 'text-red-500' : 'text-black'} onClick={toggleMobileMenu}>Home</NavLink></li>
+                    <li><NavLink to={'/about'} className={({ isActive }) => isActive ? 'text-red-500' : 'text-black'} onClick={toggleMobileMenu}>About Us</NavLink></li>
+                    <li><NavLink to={'/programs'} className={({ isActive }) => isActive ? 'text-red-500' : 'text-black'} onClick={toggleMobileMenu}>Programs and Services</NavLink></li>
+                    <li><NavLink to={'/newsandupdates'} className={({ isActive }) => isActive ? 'text-red-500' : 'text-black'} onClick={toggleMobileMenu}>News and Updates</NavLink></li>
+                    <li><NavLink to={'/donate'} className={({ isActive }) => isActive ? 'text-red-500' : 'text-black'} onClick={toggleMobileMenu}>Donate</NavLink></li>
+                    <li><NavLink to={'/contact'} className={({ isActive }) => isActive ? 'text-red-500' : 'text-black'} onClick={toggleMobileMenu}>Contact Us</NavLink></li>
+                </ul>
+            </div>
+        )}
     </nav>
-  )
+  );
 }
